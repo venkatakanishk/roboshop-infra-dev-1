d99848496f47ae667f1d93fce9203fa6b4e0b05e
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-26 03:50:38 +0530

    changed something

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -29,7 +29,7 @@ resource "terraform_data" "bootstrap" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh, mongodb"
+        "sudo sh /tmp/bootstrap.sh mongodb"
         ]
         }
 
