a6e9d62ee8512e21889168ade9ac049b8f63bb43
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-26 02:18:03 +0530

    changed something

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role" "bastion" {
 }
 resource "aws_iam_role_policy_attachment" "bastion" {
   role       = aws_iam_role.bastion.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 resource "aws_iam_instance_profile" "bastion" {
   name = "${var.project}-${var.environment}-bastion"
