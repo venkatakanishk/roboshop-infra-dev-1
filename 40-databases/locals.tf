00583599a3ee10666eee950b9276457294c85901
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-27 01:48:16 +0530

    changed something

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -9,5 +9,5 @@ locals {
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
     rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
-    redis_sg_id = data.aws_ssm_parameter.redis.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 }
