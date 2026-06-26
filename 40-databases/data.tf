dd25011e36cb0a0c536d582d34c85539886bb8c2
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-27 01:45:27 +0530

    changed something

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -23,3 +23,12 @@ data "aws_ssm_parameter" "database_subnet_ids" {
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
+data "aws_ssm_parameter" "redis_sg_id" {
+  name = "/${var.project}/${var.environment}/redis_sg_id"
+}
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project}/${var.environment}/mysql_sg_id"
+}
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
+}
